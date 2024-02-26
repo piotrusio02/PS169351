011b0b9e70d31a7cd8c285a4d8f8bfe496334464
piotrusio02 <115213217+piotrusio02@users.noreply.github.com>
2024-02-26 14:03:29 +0100

    Create zad4.c

---
+++ Instrukcje warunkowe, operator warunkowy/zad4.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a,b ,c;
+    printf("Podaj trzy liczby: ");
+    scanf("%d", &a);
+    scanf("%d", &b);
+    scanf("%d", &c);
+    int mniejsza = a < b ? a : b;
+    int najmniejsza = mniejsza < c ? mniejsza : c;
+    printf("Najmniejsza liczba to: %d", najmniejsza);
+    return 0;
+}
