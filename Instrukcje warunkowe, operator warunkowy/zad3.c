c3534c29edc67be782a4d3dfca601c12dca8df85
piotrusio02 <115213217+piotrusio02@users.noreply.github.com>
2024-02-26 13:50:24 +0100

    Create zad3.c

---
+++ Instrukcje warunkowe, operator warunkowy/zad3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a;
+    printf("Podaj ocene w skali od 1 do 5: ");
+    scanf("%d", &a);
+    if (a < 0 || a > 5) {
+        printf("ocena nie jest z skali!");
+    }
+    switch (a) {
+    case 1:
+        printf("niedostateczny!");
+        break;
+    case 2:
+        printf("dopuszczajacy!");
+        break;
+    case 3:
+        printf("dostateczny!");
+        break;
+    case 4:
+        printf("dobry!");
+        break;
+    case 5:
+        printf("bardzo dobry");
+        break;
+    }
+    return 0;
+}
