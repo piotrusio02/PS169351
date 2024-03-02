87deeae98f9cbb012db2abf45f2383f1f6e06053
piotrusio02 <115213217+piotrusio02@users.noreply.github.com>
2024-03-02 18:06:22 +0100

    Create zad6.c

---
+++ Instrukcje warunkowe, operator warunkowy/zad6.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int x, y;
+    printf("podaj pierwsza liczbe: ");
+    scanf("%d", &x);
+    printf("podaj druga liczbe: ");
+    scanf("%d", &y);
+    int suma = x + y;
+        if (suma % 2 == 0) {
+            printf("liczba: %d jest parzysta!", suma);
+        }
+        else {
+            printf("liczba: %d jest nieparzysta!", suma);
+        }
+    return 0;
+}
