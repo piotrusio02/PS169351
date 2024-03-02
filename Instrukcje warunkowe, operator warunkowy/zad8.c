a4d4d4260e9f1aaa5ad6cb4f28ba0dc5ff6503e0
piotrusio02 <115213217+piotrusio02@users.noreply.github.com>
2024-03-02 18:47:23 +0100

    Create zad8.c

---
+++ Instrukcje warunkowe, operator warunkowy/zad8.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+int main()
+{
+    int x, y;
+    printf("podaj pierwsza liczbe: ");
+    scanf("%d", &x);
+    printf("podaj druga liczbe: ");
+    scanf("%d", &y);
+    int wieksza = (x > y) ? x : y;
+    printf("wieksza liczba to: %d", wieksza);
+    return 0;
+}
