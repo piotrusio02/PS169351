2b90dfa97416bb39861c36561005c55cab1f0717
piotrusio02 <115213217+piotrusio02@users.noreply.github.com>
2024-03-02 18:55:41 +0100

    Create zad10.c

---
+++ Instrukcje warunkowe, operator warunkowy/zad10.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int x;
+    printf("podaj liczbe calkowita: ");
+    scanf("%d", &x);
+    printf((x % 2 == 0) ? "parzysta" : "nieparzysta");
+    return 0;
+}
