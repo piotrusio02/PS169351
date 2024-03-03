9a961ec13da5e5726afb1ee44544746186c5d1ff
piotrusio02 <115213217+piotrusio02@users.noreply.github.com>
2024-03-03 23:07:02 +0100

    Create zad_12.c

---
+++ Operacje wejścia, wyjścia/zad_12.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main()
+{
+    int x;
+    printf(" Podaj liczbe calkowita: ");
+    scanf(" %d", &x);
+    printf(" wartosc bezwzgledna twojej liczby: %d", abs(x));
+    return 0;
+}
