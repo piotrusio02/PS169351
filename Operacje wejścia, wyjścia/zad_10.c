700eee024ff4763d42cf2f7b910ea9856ea26341
piotrusio02 <115213217+piotrusio02@users.noreply.github.com>
2024-03-03 22:49:08 +0100

    Create zad_10.c

---
+++ Operacje wejścia, wyjścia/zad_10.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf(" Specjalne znaki: \\t (tabulacja), \\n (nowa linia), %% (procent), \\\\ (ukosnik wsteczny)." );
+    return 0;
+}
