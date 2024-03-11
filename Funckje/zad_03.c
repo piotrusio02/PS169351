fc7eae56c2ca4767181d2deb69354e185ec6ae8f
piotrusio02 <115213217+piotrusio02@users.noreply.github.com>
2024-03-11 21:51:27 +0100

    Create zad_03.c

---
+++ Funckje/zad_03.c
@@ -0,0 +1,25 @@
+/* Napisz funkcję calculateFactorial, której argumentem jest liczba całkowita nieujemna.
+Funkcja ma zwracać wartość silni liczby przekazanej jako argument funkcji, obliczoną
+metodą nierekurencyjną. Stwórz przypadek testowy. */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int calculateFactorial(int x){
+    int silnia = 1;
+    for(int i = 1;i<x+1;i++){
+        silnia = silnia * i;
+    }
+    return silnia;
+
+
+}
+
+int main(){
+
+    int x;
+    printf("podaj liczbe: ");
+    scanf("%d", &x);
+    printf("%d",calculateFactorial(x));
+    return 0;
+}
