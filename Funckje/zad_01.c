59dd1f41256f7abebfe9a51487d27317de06a7e9
piotrusio02 <115213217+piotrusio02@users.noreply.github.com>
2024-03-11 14:18:22 +0100

    Create zad_01.c

---
+++ Funckje/zad_01.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int sumTwoNumbers(int x, int y){
+    return x+y;
+}
+
+int main(){
+printf("%d + %d = %d\n",4,6, sumTwoNumbers(4,6));
+printf("%d + %d = %d\n",5,25, sumTwoNumbers(5,25));
+return 0;
+}
