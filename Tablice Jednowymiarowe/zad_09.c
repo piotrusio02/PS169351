8c05fc9d0c05124f9a9ff54d75a38a6cccb6bec1
piotrusio02 <115213217+piotrusio02@users.noreply.github.com>
2024-04-07 11:35:32 +0200

    Create zad_09.c

---
+++ Tablice Jednowymiarowe/zad_09.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int minValue(int n, int tab[]){
+    int ind = 0;
+    for (int i = 0;i<n;i++){
+        if (tab[ind]< tab[i]){
+            ind = i;
+        }}
+    return ind;
+
+    }
+
+
+int main() {
+    int tab1[] = {111,7,1036,5,2,34,1036,11};
+    printf("indeks najwyzszej wartosci w tablicy to: %d", minValue(8, tab1));
+    return 0;
+}
