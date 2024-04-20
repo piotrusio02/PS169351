3bb03b280e67c6758d38b34a7f8af9bd282bee33
piotrusio02 <115213217+piotrusio02@users.noreply.github.com>
2024-04-20 22:03:25 +0200

    Create zad_08.c

---
+++ Napisy/zad_08.c
@@ -0,0 +1,24 @@
+/* "Zakładamy, że w tablicy docelowej jest wystarczająco dużo miejsca" rozumiem,
+  że mogłem podać dowolną wielkość, taką by po prostu napewno była większa */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void strCopyNew(char txt[], char txt2[]){
+    int i;
+    for(i=0;txt[i] != 0; i++){
+        txt2[i] = txt[i];
+    }}
+
+
+int main()
+{
+    char napis[]= "5aATo JeSt TeSt";
+    char przepisz[100];
+    printf("napisz: %s\n", napis);
+    printf("przepisz: %s\n", przepisz);
+    strCopyNew(napis, przepisz);
+    printf("\nnapisz: %s", napis);
+    printf("\nprzepisz: %s\n", przepisz);
+    return 0;
+}
