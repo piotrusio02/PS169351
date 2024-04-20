aa2111755ec9d5764152c8e9651d00fccb94fc86
piotrusio02 <115213217+piotrusio02@users.noreply.github.com>
2024-04-20 14:24:03 +0200

    Update zad_01.c

--- Napisy/zad_01.c
+++ Napisy/zad_01.c
@@ -50,3 +50,25 @@ int main()
     printf("%d\n", length("Zadanie"));
     return 0;
 }
+
+
+/* MOJE ROZWIAZANIE W DOMU */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int length(char * napis){
+    int wynik;
+    while(*napis) {
+        wynik++;
+        napis++;;
+    }
+    return wynik;
+}
+
+int main()
+{
+    char * napis = "to jest 0 czy 1?";
+    printf("%d", length(napis)); /* zwraca 16 */
+    return 0;
+}
