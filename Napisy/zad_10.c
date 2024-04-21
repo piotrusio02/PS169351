f41e29bb55932a6417d68768e2349b9ac5393587
piotrusio02 <115213217+piotrusio02@users.noreply.github.com>
2024-04-21 09:38:25 +0200

    Create zad_10.c

---
+++ Napisy/zad_10.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void strConcat(char nap1[], char nap2[], char nap3[]){
+    int i;
+    int temp = 0;
+    for(i = 0; nap1[i]!=0;i++){
+        nap3[i] = nap1[i];
+        temp++;
+    }
+    for(i=temp;nap2[i-temp] != 0; i++){
+        nap3[i] = nap2[i-temp];
+    }
+}
+
+int main()
+{
+    char napis[]= "To je";
+    char nap2[] = "st test";
+    char nap3[100];
+    strConcat(napis, nap2, nap3);
+    printf("%s", nap3);
+    return 0;
+}
