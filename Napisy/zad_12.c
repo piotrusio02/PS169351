84233345c6f932b1b4e7868c8e369ab7cabf645d
piotrusio02 <115213217+piotrusio02@users.noreply.github.com>
2024-04-21 14:31:21 +0200

    Create zad_12.c

---
+++ Napisy/zad_12.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void rmDigits(char nap[]){
+    int i;
+    for(i=0;nap[i] != 0;i++){
+        if(nap[i] >= 48 && nap[i] <= 57){
+            for(int j=i;nap[j] != 0;j++){
+                nap[j] = nap[j+1];
+            }
+        i = i-1;
+    }}
+}
+
+
+int main()
+{
+    char napis[] = "t3o je34st5 te2st45567";
+    rmDigits(napis);
+    printf("%s", napis); /* to jest test */
+    return 0;
+}
